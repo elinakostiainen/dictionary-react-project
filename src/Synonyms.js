6f6f62d6249c150cee892bb88f1f2f2c3e1a75fc
Elina Kostiainen <92241145+elinakostiainen@users.noreply.github.com>
2022-03-10 13:48:01 +1000

    Styled list and fixed list incorrect spelling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Synonyms.css"
 
 export default function Synonyms (props){
     if (props.synonyms){
                 return (
                     <ul className="Synonyms">
                         {props.synonyms.map(function(synonym, index){
-                            return <il key={index}>{synonym}</il>;
+                            return <li key={index}>{synonym}</li>;
                         })}
                     </ul>
                 );
