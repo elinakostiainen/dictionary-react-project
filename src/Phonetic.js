c494d991c65904159cd01af6e5fa1b600dd7d97a
Elina Kostiainen <92241145+elinakostiainen@users.noreply.github.com>
2022-03-10 13:42:33 +1000

    Fixed error with rel

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,9 +4,8 @@ import "./Phonetic.css"
 export default function Phonetic(props){
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">Listen</a>
-            <br />
-            {props.phonetic.text}
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
+            <span className="text">{props.phonetic.text}</span>
         </div>
     );
 }
