15ab4a1d835fa7156a7e8793bb6157dc95be8ea2
Elina Kostiainen <92241145+elinakostiainen@users.noreply.github.com>
2022-03-11 13:25:32 +1000

    Added photos with photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,33 @@ import React, {useState} from "react"
 import axios from "axios"
 import Results from "./Results"
 import "./Dictionary.css"
+import Photos from "./Photos"
 
 export default function Dictionary(props){
 let [searchWord, setSearchWord] = useState(props.defaultWord);
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState (false);
+let [photos, setPhotos] = useState (null);
 
-function handleResponse(response){
+function handleDictionResponse(response){
     setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response){
+    setPhotos(response.data.photos);
 }
 
 function search(){
 // documentation url = https://dictionaryapi.dev/
 let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`
 
-axios.get(apiUrl).then(handleResponse)
+axios.get(apiUrl).then(handleDictionResponse)
+
+let pexelsApiKey = "563492ad6f91700001000001c4ffd81850b842229731236912883380";
+let headers = {Authorization: `Beaner ${pexelsApiKey}`}
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=12`
+
+axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
 }
 
 function handleSubmit(event){
@@ -44,6 +56,7 @@ if (loaded)
                 <input type="search" placeholder="Search for a word" autoFocus={true} onChange={handleSearchWordChange} defaultValue={props.defaultWord}/>
             </form>
             <Results results={results}/>
+            <Photos photos={photos}/>
         </div>
     )
 } else {
