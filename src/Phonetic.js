52c0992f50365b48eef847cc0fc9c3f5168de6a9
Elina Kostiainen <92241145+elinakostiainen@users.noreply.github.com>
2022-03-11 13:29:52 +1000

    Added React Audio Player for Phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ReactAudioPlayer from 'react-audio-player';
 import "./Phonetic.css"
 
 export default function Phonetic(props){
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
+            <ReactAudioPlayer src={props.phonetic.audio} controls
+/>
             <span className="text">{props.phonetic.text}</span>
         </div>
     );
