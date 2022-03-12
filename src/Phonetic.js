946868954926e105da5ac5b7f2987467dcf34aeb
Elina Kostiainen <92241145+elinakostiainen@users.noreply.github.com>
2022-03-12 10:27:15 +1000

    Removed react audio player

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactAudioPlayer from 'react-audio-player';
 import "./Phonetic.css"
 
 export default function Phonetic(props){
     return (
         <div className="Phonetic">
             <div className="audio">
-            <ReactAudioPlayer src={props.phonetic.audio} controls />
+                <audio  src={props.phonetic.audio} controls>
+                </audio>
             </div>
             <div className="text mt-3">{props.phonetic.text}</div>
         </div>
